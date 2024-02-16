9f17a6e0674268b8ddf31ca72a7ed3edcd854c77
Himanshu Jagdish Sheth <himanshu.sheth@gmail.com>
2024-02-16 16:09:15 +0530

    Code: URL health checking and Web Scraping scenario

---
+++ tests/url-health-checking/test_async_url_health_check.py
@@ -0,0 +1,31 @@
+import sys
+from pageobject.locators import locators
+from pageobject.locators import *
+from pageobject.helpers import helpers
+from pageobject.helpers import *
+
+sys.path.append(sys.path[0] + "/../../..")
+
+class TestAsyncOps:
+    @pytest.mark.asyncio
+    @pytest.mark.run(order=1)
+    async def test_async_url_access(self, driver) -> list:
+        meta_data_arr = []
+        driver.get(locators.test_playground_url)
+
+        driver.maximize_window()
+
+        meta_data_arr = helpers.scrap_playground_url(driver)
+
+        async def check_status(session, url):
+            async with session.get(url) as response:
+                status_code = response.status
+                print(url + " status = " + str(status_code) + " ")
+                return status_code
+
+        async with aiohttp.ClientSession() as session:
+            tasks = [check_status(session, url) for url in meta_data_arr]
+            status_codes = await asyncio.gather(*tasks)
+
+        for status_code, url in zip(status_codes, meta_data_arr):
+            assert status_code == 200, f"Failed for URL: {url}, Status Code: {status_code}"
---
+++ tests/web-scraping/test_async_ecommerce_scrapping.py
@@ -0,0 +1,63 @@
+import asyncio
+import aiohttp
+import sys
+import ssl
+import certifi
+from pprint import pprint
+from bs4 import BeautifulSoup
+
+sys.path.append(sys.path[0] + "/../../..")
+
+from pageobject.locators import locators
+from pageobject.helpers import helpers
+
+async def fetch(url, session):
+    async with session.get(url) as response:
+        return await response.text()
+
+# Encountered the below error
+# aiohttp.client_exceptions.ClientConnectorCertificateError: Cannot connect to 
+# host ecommerce-playground.lambdatest.io:443 ssl:True 
+# [SSLCertVerificationError: (1, '[SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed: 
+# unable to get local issuer certificate (_ssl.c:1000)')]
+
+# Solution: https://stackoverflow.com/a/66842057/126105
+
+async def scrap_ecommerce(url):
+    ssl_context = ssl.create_default_context(cafile=certifi.where())
+    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl_context=ssl_context)) as session:
+        html = await fetch(url, session)
+        soup = BeautifulSoup(html, 'html.parser')
+
+        rows = soup.select('.product-layout.product-grid.no-desc.col-xl-4.col-lg-4.col-md-4.col-sm-6.col-6')
+        meta_data_arr = []
+
+        for row in rows:
+            link = row.find("a", class_='carousel d-block slide')
+            name = row.find("h4", class_='title')
+            price = row.find("span", class_='price-new')
+
+            meta_data_dict = {
+                'product link': link.get('href'),
+                'product name': name.get_text(),
+                'product price': price.get_text()
+            }
+            
+            meta_data_arr.append(meta_data_dict)
+
+        return meta_data_arr
+
+async def main():
+    base_url = locators.test_bs4_url
+    tasks = [scrap_ecommerce(f"{base_url}&page={i}") for i in range(1, 6)]
+    results = await asyncio.gather(*tasks)
+
+    for i, result in enumerate(results, 1):
+        print(f"Product Page = {base_url}&page={i}")
+        print("*********************************************************************************************************")
+        helpers.print_scrapped_content(result)
+        print()
+
+if __name__ == '__main__':
+    output = asyncio.run(main())
+    print(output)
